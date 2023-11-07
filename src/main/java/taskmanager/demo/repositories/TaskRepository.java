1c351134294188b79df6997ce8e32876ae38a3db
Jayabharatha Reddy Sadanagiri <Jayabharathreddys@gmail.com>
2023-11-07 17:59:16 +0530

    all
    Signed-off-by: Jayabharatha Reddy Sadanagiri <Jayabharathreddys@gmail.com>

--- src/main/java/taskmanager/demo/repositories/TaskRepository.java
+++ src/main/java/taskmanager/demo/repositories/TaskRepository.java
@@ -1,2 +1,12 @@
-package taskmanager.demo.repositories;public class TaskRepository {
+package taskmanager.demo.repositories;
+
+import org.springframework.stereotype.Repository;
+import taskmanager.demo.entities.TaskEntity;
+
+import java.util.ArrayList;
+
+@Repository
+public class TaskRepository {
+    private ArrayList<TaskEntity> tasks = new ArrayList<>();
+    private int taskId = 1;
 }
